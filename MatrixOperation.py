e4368c261f001f12d7d24226cdbf2b4972d91872
Titov Ihor <44226375+lurak@users.noreply.github.com>
2020-05-08 15:29:24 +0300

    Add documentation

--- MatrixOperation.py
+++ MatrixOperation.py
@@ -1,11 +1,27 @@
 class MatrixOperation:
+    """
+    Help class for some basic matrix operation.
+    """
     def __init__(self, matrix):
+        """
+        Initialization of a matrix.
+        :param matrix: 2D list of numbers.
+        """
         self.matrix = matrix
 
     def change_matrix(self, matrix):
+        """
+        Change a matrix for a class.
+        :param matrix: 2D list of numbers.
+        :return: None
+        """
         self.matrix = matrix
 
     def transpose(self):
+        """
+        Return transpose matrix to the given one.
+        :return: 2D list of numbers
+        """
         tr = list()
         row = list()
         for i in range(len(self.matrix[0])):
