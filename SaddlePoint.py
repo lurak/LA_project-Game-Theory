e1dde24b40ebd776b46e7a3a526bf9b6e2ad6c93
Titov Ihor <44226375+lurak@users.noreply.github.com>
2020-05-18 13:42:52 +0300

    Finished SaddlePoint

--- SaddlePoint.py
+++ SaddlePoint.py
@@ -13,6 +13,7 @@ class SaddlePoint:
         :param: matrix: 2D list of number
         """
         self.matrix = matrix
+        self.flag = False
 
     def find_max_min_row(self):
         """
@@ -59,26 +60,44 @@ class SaddlePoint:
         return row, column
 
     @staticmethod
-    def row_domination(matrix):
+    def row_domination(matrix, column=False):
         """
         Function which help to find dominated strategy, find out such rows in the matrix.
         :param matrix: 2D list
         :return: 2D list
         """
-        counter = 0
+        counter_1, counter_2 = 0, 0
         weak = list()
         for i in range(len(matrix)):
             for j in range(i + 1, len(matrix)):
                 for l in range(len(matrix[i])):
-                    if matrix[i][l] <= matrix[j][l]:
-                        counter += 1
-                if counter == len(matrix[i]):
-                    counter = 0
-                    weak.append(matrix[i])
+                    if matrix[i][l] <= matrix[j][l] and not column:
+                        counter_1 += 1
+                    if matrix[i][l] >= matrix[j][l] and not column:
+                        counter_2 += 1
+                    if matrix[i][l] >= matrix[j][l] and column:
+                        counter_1 +=1
+                    if matrix[i][l] <= matrix[j][l] and column:
+                        counter_2 += 1
+                if counter_1 == len(matrix[i]):
+                    counter_1, counter_2 = 0, 0
+                    if matrix[i] not in weak:
+                        weak.append(matrix[i])
                     break
-                counter = 0
-        for i in range(len(weak)):
-            matrix.remove(weak[i])
+                if counter_2 == len(matrix[i]):
+                    counter_1, counter_2 = 0, 0
+                    if matrix[j] not in weak:
+                        weak.append(matrix[j])
+                    break
+                counter_1, counter_2 = 0, 0
+        i = 0
+        while True:
+            if matrix[i] in weak:
+                weak.remove(matrix[i])
+                matrix.remove(matrix[i])
+            i += 1
+            if not weak:
+                break
         return matrix
 
     @staticmethod
@@ -97,10 +116,10 @@ class SaddlePoint:
         Function which detect all dominated strategies and delete them from matrix.
         :return: None
         """
-        self.matrix = self.row_domination(self.matrix)
+        self.row_domination(self.matrix)
         m = MatrixOperation(self.matrix)
         tr = m.transpose()
-        tr = self.row_domination(tr)
+        tr = self.row_domination(tr, True)
         m.change_matrix(tr)
         self.matrix = m.transpose()
 
@@ -114,16 +133,15 @@ class SaddlePoint:
         m_2 = self.find_min_max_column()
         if m_1 == m_2:
             row, column = self.optimal_choice(m_1)
-            return "Saddle point: " + str(m_1) + '  Optimal choice for 1st player: ' + self.convector(row) + \
-                   ' Optimal choice for 2nd player ' + self.convector(column)
+            return "Saddle point: " + str(m_1) + '  Optimal choice for 1st player: ' + \
+                              self.convector(row)+ ' Optimal choice for 2nd player: ' + self.convector(column)
         else:
+            self.flag = True
             return "There is no saddle point"
 
 
 if __name__ == "__main__":
-    s_1 = SaddlePoint([[-1, -1],
-                       [-1.5, 0],
-                       [0.5, 0],
-                       [0, 1]])
-    #print(s_1.convector([1,2,3]))
+    s_1 = SaddlePoint([[4,4,10],
+                       [2,3,1],
+                       [6,5,7]])
     print(s_1.saddle_point())
